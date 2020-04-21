82310f96f0535b84d06a8e3280abfef8f7099734
Srikar Reddy <saisrikarreddy101@gmail.com>
2020-04-21 17:45:45 +0530

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       Srikar Reddy
-      </header>
-    </div>
-  );
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+import image from './images/image.png';
+
+class App extends React.Component<any,any> {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData(this.state.country);
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country:any) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
 }
 
 export default App;
